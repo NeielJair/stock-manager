bc4badaf92085c6e6b34a9785299e708e0cef7de
estebanmaxposse <estebanposse98@gmail.com>
2023-05-02 23:07:38 -0300

    created person model

--- stock_app/models.py
+++ stock_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    dni = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
